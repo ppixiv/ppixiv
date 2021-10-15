5638660da049c971e3cecc761c14fe281340f60f
ppixiv <ppixiv@users.noreply.github.com>
2021-10-15 03:06:07 -0500

    Rename session settings to sticky settings, and add real session settings which are stored to sessionStorage.

--- src/settings.js
+++ src/settings.js
@@ -3,6 +3,7 @@
 // Get and set values in localStorage.
 ppixiv.settings = class
 {
+    static sticky_settings = { };
     static session_settings = { };
 
     static get_change_callback_list(key)
@@ -15,37 +16,70 @@ ppixiv.settings = class
         return callbacks;
     }
 
+    // Configure settings.  This is used for properties of settings that we need to
+    // know at startup, so we know where to find them.
+    //
+    // Sticky settings are saved and loaded like other settings, but once a setting is loaded,
+    // changes made by other tabs won't affect this instance.  This is used for things like zoom
+    // settings, where we want to store the setting, but we don't want each tab to clobber every
+    // other tab every time it's changed.
+    //
+    // Session settings are stored in sessionStorage instead of localStorage.  These are
+    // local to the tab.  They'll be copied into new tabs if a tab is duplicated, but they're
+    // otherwise isolated, and lost when the tab is closed.
+    static configure(key, {sticky=false, session=false})
+    {
+        if(sticky)
+        {
+            // Create the key if it doesn't exist.
+            if(settings.sticky_settings[key] === undefined)
+                settings.sticky_settings[key] = null;
+        }
+
+        if(session)
+            this.session_settings[key] = true;
+    }
+
+    static _get_storage_for_key(key)
+    {
+        if(this.session_settings[key])
+            return sessionStorage;
+        else
+            return localStorage;
+    }
+
     static _get_from_storage(key, default_value)
     {
-        key = "_ppixiv_" + key;
+        let storage = this._get_storage_for_key(key);
 
-        if(!(key in localStorage))
+        key = "_ppixiv_" + key;
+        if(!(key in storage))
             return default_value;
 
-        let result = localStorage[key];
+        let result = storage[key];
         try {
             return JSON.parse(result);
         } catch(e) {
-            // Recover from invalid values in localStorage.
+            // Recover from invalid values in storage.
             console.warn(e);
             console.log("Removing invalid setting:", result);
-            delete localStorage.storage_key;
+            delete storage.storage_key;
             return default_value;
         }
     }
 
     static get(key, default_value)
     {
-        // If this is a session setting and we've already read it, use our loaded value.
-        if(settings.session_settings[key])
-            return settings.session_settings[key];
+        // If this is a sticky setting and we've already read it, use our loaded value.
+        if(settings.sticky_settings[key])
+            return settings.sticky_settings[key];
 
         let result = settings._get_from_storage(key, default_value);
 
-        // If this is a session setting, remember it for reuse.  This will store the default value
+        // If this is a sticky setting, remember it for reuse.  This will store the default value
         // if there's no stored setting.
-        if(settings.session_settings[key] !== undefined)
-            settings.session_settings[key] = result;
+        if(settings.sticky_settings[key] !== undefined)
+            settings.sticky_settings[key] = result;
 
         return result;
     }
@@ -64,33 +98,24 @@ ppixiv.settings = class
 
     static set(key, value)
     {
+        let storage = this._get_storage_for_key(key);
+
         // JSON.stringify incorrectly serializes undefined as "undefined", which isn't
         // valid JSON.  We shouldn't be doing this anyway.
         if(value === undefined)
             throw "Key can't be set to undefined: " + key;
 
-        // If this is a session setting, replace its value.
-        if(settings.session_settings[key] !== undefined)
-            settings.session_settings[key] = value;
+        // If this is a sticky setting, replace its value.
+        if(settings.sticky_settings[key] !== undefined)
+            settings.sticky_settings[key] = value;
 
         var setting_key = "_ppixiv_" + key;
 
         var value = JSON.stringify(value);
-        localStorage[setting_key] = value;
+        storage[setting_key] = value;
 
         // Call change listeners for this key.
         settings.get_change_callback_list(key).call(key);
-    }
-
-    // Mark a setting as per-session.  These are saved and loaded like other settings, but
-    // once a setting is loaded, changes made by other tabs won't affect this instance.
-    // This is used for things like zoom settings, where we want to store the setting, but
-    // we don't want each tab to clobber every other tab every time it's changed.
-    static set_per_session(key)
-    {
-        // Create the key if it doesn't exist.
-        if(settings.session_settings[key] === undefined)
-            settings.session_settings[key] = null;
     }
 
     static register_change_callback(key, callback)
@@ -104,4 +129,7 @@ ppixiv.settings = class
     }
 }
 
+// Register settings.
+ppixiv.settings.configure("zoom-mode", { sticky: true });
+ppixiv.settings.configure("zoom-level", { sticky: true });
 
