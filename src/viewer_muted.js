96c765e9bf59a8c0e0e65af3e32353d81ec5433f
ppixiv <ppixiv@users.noreply.github.com>
2021-11-02 23:00:52 -0500

    Move the muted template into viewer_muted now that it's a widget.

--- src/viewer_muted.js
+++ src/viewer_muted.js
@@ -5,18 +5,25 @@ ppixiv.viewer_muted = class extends ppixiv.viewer
 {
     constructor(options)
     {
-        super(options);
-
-        // Create the display.
-        this.root = helpers.create_from_template(".template-muted");
-        container.appendChild(this.root);
+        super({...options, template: `
+            <div class=mute-display>
+                <img class=muted-image>
+                <div class=muted-text>
+                    <Span>Muted:</span>
+                    <span class=muted-label></span>
+                    <div class=view-muted-image>
+                        View image
+                    </div>
+                </div>
+            </div>
+        `});
 
         this.load();
     }
 
     async load()
     {
-        this.root.querySelector(".view-muted-image").addEventListener("click", (e) => {
+        this.container.querySelector(".view-muted-image").addEventListener("click", (e) => {
             let args = helpers.args.location;
             args.hash.set("view-muted", "1");
             helpers.set_page_url(args, false /* add_to_history */, "override-mute");
@@ -30,13 +37,13 @@ ppixiv.viewer_muted = class extends ppixiv.viewer
         
         // Show the user's avatar instead of the muted image.
         let user_info = await image_data.singleton().get_user_info(this.illust_data.userId);
-        var img = this.root.querySelector(".muted-image");
+        var img = this.container.querySelector(".muted-image");
         img.src = user_info.imageBig;
 
         let muted_tag = muting.singleton.any_tag_muted(this.illust_data.tagList);
         let muted_user = muting.singleton.is_muted_user_id(this.illust_data.userId);
 
-        let muted_label = this.root.querySelector(".muted-label");
+        let muted_label = this.container.querySelector(".muted-label");
         if(muted_tag)
             tag_translations.get().set_translated_tag(muted_label, muted_tag);
         else if(muted_user)
@@ -47,7 +54,7 @@ ppixiv.viewer_muted = class extends ppixiv.viewer
     {
         super.shutdown();
 
-        this.root.parentNode.removeChild(this.root);
+        this.container.parentNode.removeChild(this.container);
     }
 }
 
