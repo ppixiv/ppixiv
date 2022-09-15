5fd0a77e8cd05f1fb88a5f38a6cfae4e5a2289dd
ppixiv <ppixiv@users.noreply.github.com>
2022-09-15 13:50:26 -0500

    Improve opening local vview windows.

--- vview/shell/open_path.py
+++ vview/shell/open_path.py
@@ -1,8 +1,27 @@
-import sys, webbrowser, urllib.parse
+import subprocess, sys, webbrowser, urllib.parse, winreg
 from pathlib import Path
 
 # vview.shell.register registers this for file type associations.
 # This is normally called through VView.exe.
+
+def _application_path(name):
+    try:
+        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, rf'Software\Microsoft\Windows\CurrentVersion\App Paths\{name}')
+        return winreg.QueryValueEx(key, None)[0]
+    except FileNotFoundError as e:
+        return None
+
+def _chrome_path():
+    """
+    Return the path to the Chrome executable.
+    """
+    return _application_path('chrome.exe')
+
+def _firefox_path():
+    """
+    Return the path to the Firefox executable.
+    """
+    return _application_path('firefox.exe')
 
 def open_path(path):
     """
@@ -11,7 +30,29 @@ def open_path(path):
     path = Path(path)
     url_path = str(path).replace('\\', '/')
     url = 'http://127.0.0.1:8235/open/' + urllib.parse.quote(url_path)
-    webbrowser.open(url)
+
+    # Don't use the webbrowser module.  The Windows implementation is an afterthought, and
+    # the module should never have been added to the Python core library in its current state.
+    if True:
+        # Open directly with Chrome.  This lets us open in a new window.  It would be nicer to open without
+        # browser chrome, but Chrome won't do that unless we open in a separate profile, which is worse.
+        chrome_path = _chrome_path()
+        args = [
+            chrome_path,
+            '--new-window',
+            url,
+        ]
+        subprocess.Popen(args)
+    elif True:
+        firefox_path = _firefox_path()
+        args = [
+            firefox_path,
+            '-new-window',
+            url,
+        ]
+        subprocess.Popen(args)
+    else:
+        webbrowser.open_new(url)
 
 def go():
     if len(sys.argv) < 2:
