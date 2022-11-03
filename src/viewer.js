6f8b7d44c52678fe915e2abe7293b62b2f279b0e
ppixiv <ppixiv@users.noreply.github.com>
2022-11-03 16:05:18 -0500

    Remove unused viewer.hide_image.

--- src/viewer.js
+++ src/viewer.js
@@ -25,13 +25,6 @@ ppixiv.viewer = class extends widget
     set active(value) { this._active = value; }
     get active() { return this._active; }
 
-    // If an image is displayed, clear it.
-    //
-    // This is only used with the illust viewer when changing manga pages in cases
-    // where we don't want the old image to be displayed while the new one loads.
-    set hide_image(value) { }
-    get hide_image() { return false; }
-
     // Return the amount of space that should be reserved by the mobile UI for this view.
     get bottom_reservation() { return "0px"; }
 }
