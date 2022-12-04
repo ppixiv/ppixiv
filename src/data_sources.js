cd24d8a6b40bc51423d25e1fe6ed29288a06bc19
ppixiv <ppixiv@users.noreply.github.com>
2022-12-04 17:05:42 -0600

    Rework the rankings menu.

--- src/data_sources.js
+++ src/data_sources.js
@@ -1564,64 +1564,218 @@ ppixiv.data_sources.rankings = class extends data_source
         this.ui = new ppixiv.widget({
             ...options,
             template: `
-            <div class=box-button-row>
+            <div>
                 <div class=box-button-row>
-                    ${ helpers.create_box_link({label: "Next day", popup: "Show the next day",     data_type: "new-illust-type-illust", classes: ["nav-tomorrow"] }) }
-                    <span class=nav-today style="margin: 0 0.25em;"></span>
-                    ${ helpers.create_box_link({label: "Previous day", popup: "Show the previous day",     data_type: "new-illust-type-illust", classes: ["nav-yesterday"] }) }
+                    <div class=box-button-row>
+                        ${ helpers.create_box_link({label: "Next day", popup: "Show the next day",     data_type: "new-illust-type-illust", classes: ["nav-tomorrow"] }) }
+                        <span class=nav-today style="margin: 0 0.25em;"></span>
+                        ${ helpers.create_box_link({label: "Previous day", popup: "Show the previous day",     data_type: "new-illust-type-illust", classes: ["nav-yesterday"] }) }
+                    </div>
+
+                    <div class=box-button-row>
+                        ${ helpers.create_box_link({label: "All",           popup: "Show all works",           data_type: "content-all" }) }
+                        ${ helpers.create_box_link({label: "Illustrations", popup: "Show illustrations only",  data_type: "content-illust" }) }
+                        ${ helpers.create_box_link({label: "Animations",    popup: "Show animations only",     data_type: "content-ugoira" }) }
+                        ${ helpers.create_box_link({label: "Manga",         popup: "Show manga only",          data_type: "content-manga" }) }
+                    </div>
                 </div>
 
-                <div class="checked-links box-button-row">
-                    ${ helpers.create_box_link({label: "All",           popup: "Show all works",           data_type: "content-all" }) }
-                    ${ helpers.create_box_link({label: "Illustrations", popup: "Show illustrations only",  data_type: "content-illust" }) }
-                    ${ helpers.create_box_link({label: "Animations",    popup: "Show animations only",     data_type: "content-ugoira" }) }
-                    ${ helpers.create_box_link({label: "Manga",         popup: "Show manga only",          data_type: "content-manga" }) }
-                </div>
-
-                <div class="checked-links box-button-row">
-                    ${ helpers.create_box_link({label: "Daily",         popup: "Daily rankings",           data_type: "mode-daily" }) }
-                    ${ helpers.create_box_link({label: "R18",           popup: "Show R18 works (daily only)",     data_type: "mode-daily-r18", classes: ["r18"] }) }
-                    ${ helpers.create_box_link({label: "R18G",          popup: "Show R18G works (weekly only)",   data_type: "mode-r18g", classes: ["r18g"] }) }
-                    ${ helpers.create_box_link({label: "AI",            popup: "Show AI works (daily only)",      data_type: "mode-ai" }) }
-                    ${ helpers.create_box_link({label: "AI R18",        popup: "Show R18 AI works (daily only)",  data_type: "mode-ai-r18", classes: ["r18"] }) }
-                    ${ helpers.create_box_link({label: "Weekly",        popup: "Weekly rankings",          data_type: "mode-weekly" }) }
-                    ${ helpers.create_box_link({label: "Monthly",       popup: "Monthly rankings",         data_type: "mode-monthly" }) }
-                    ${ helpers.create_box_link({label: "Rookie",        popup: "Rookie rankings",          data_type: "mode-rookie" }) }
-                    ${ helpers.create_box_link({label: "Original",      popup: "Original rankings",        data_type: "mode-original" }) }
-                    ${ helpers.create_box_link({label: "Male",          popup: "Popular with men",         data_type: "mode-male" }) }
-                    ${ helpers.create_box_link({label: "Female",        popup: "Popular with women",       data_type: "mode-female" }) }
-                </div>
+                <div class="box-button-row modes"></div>
             </div>
         `});
+
+        /*
+         * Pixiv has a fixed list of rankings, but it displays them as a set of buttons
+         * based on the current selection, showing which rankings are available in the current
+         * category.
+         *     
+         * These are the available ranking modes, and whether they're available in overall,
+         * content=illust/manga (these have the same selections) and content=ugoira, and
+         * whether there are R18 rankings.  R18 rankings have the same mode name with "_r18"
+         * appended.  (Except for AI which puts it in the middle, because Pixiv.)
+         *
+         * Be careful: Pixiv's UI has buttons in some filters that don't actually exist in the
+         * mode it's in, which actually redirect out of the content mode, such as "popular among
+         * male users" in "Illustrations" mode which actually goes back to "Overall".
+         *
+         * o: overall (all) o*: overall R18      o**: overall R18G
+         * i: illust/manga  i*: illust/manga R18 i**: illust/manga R18G
+         * u: ugoira        u*: ugoira R18
+         */
+        let ranking_types = {
+            //                       Overall    Illust     Ugoira
+            //                            R18        R18        R18       
+            "daily":     { content: ["o", "o*", "i", "i*", "u", "u*"],                label: "Daily",    popup: "Daily rankings",  },
+
+            // Weekly also has "r18g" for most content types.
+            "weekly":    { content: ["o", "o*", "i", "i*", "u", "u*", "o**", "i**"],  label: "Weekly",   popup: "Weekly rankings",  },
+            "monthly":   { content: ["o",       "i"],                                 label: "Monthly",  popup: "Monthly rankings",  },
+            "rookie":    { content: ["o",       "i"],                                 label: "Rookie",   popup: "Rookie rankings" },
+            "original":  { content: ["o"],                                            label: "Original", popup: "Original rankings" },
+            "daily_ai":  { content: ["o", "o*"],                                      label: "AI",       popup: "Show AI works" },
+            "male":      { content: ["o", "o*"],                                      label: "Male",     popup: "Popular with men"      },
+            "female":    { content: ["o", "o*"],                                      label: "Female",   popup: "Popular with women"       },
+        };
+
+        // Given a content selection ("all", "illust", "manga", "ugoira") and an ages selection, return
+        // the shorthand key for this combination, such as "i*".
+        function content_key_for(content, ages)
+        {
+            let keys = { "all": "o", "illust": "i", "manga": "i" /* same as illust */, "ugoira": "u" };
+            let content_key = keys[content];
+
+            // Append * for r18 and ** for r18g.
+            if(ages == "r18")
+                content_key += "*";
+            else if(ages == "r18g")
+                content_key += "**";
+
+            return content_key;
+        }
+
+        // Given a mode ("daily") and an ages selection ("r18"), return the combined mode,
+        // eg. "daily_r18".
+        function mode_with_ages(mode, ages)
+        {
+            if(ages == "r18")
+                mode += "_r18"; // daily_r18
+            else if(ages == "r18g")
+                mode += "_r18g"; // daily_r18g
+
+            // Seriously, guys?
+            if(mode == "daily_ai_r18")
+                mode = "daily_r18_ai";
+            else if(mode == "weekly_r18g")
+                mode = "r18g";
+
+            return mode;
+        }
+
+        let current_args = helpers.args.location;
+
+        // The current content type: all, illust, manga, ugoira
+        let current_content = current_args.query.get("content") || "all";
+
+        // The current mode: daily, weekly, etc.
+        let current_mode = current_args.query.get("mode") || "daily";
+        if(current_mode == "r18g") // work around Pixiv inconsistency
+            current_mode = "weekly_r18g";
+
+        // "all", "r18", "r18g"
+        let current_ages = current_mode.indexOf("r18g") != -1? "r18g":
+            current_mode.indexOf("r18") != -1? "r18":"all";
+        
+        // Strip _r18 or _r18g out of current_mode, so current_mode is the base mode, ignoring the
+        // ages selection.
+        current_mode = current_mode.replace("_r18g", "").replace("_r18", "");
+
+        // The key for the current mode:
+        let content_key = content_key_for(current_content, current_ages);
+        console.log(`Rankings content mode: ${current_content}, ages: ${current_ages}, key: ${content_key}`);
+
+        // Set up the content links.
+        for(let content of ["all", "illust", "ugoira", "manga"])
+        {
+            this.set_item(this.ui.container, {
+                type: "content-" + content, // content-all, content-illust, etc
+                fields: {content},
+                default_values: {content: "all"},
+                adjust_url: (args) => {
+                    if(content == current_content)
+                        return;
+
+                    // If the current mode and ages combination doesn't exist in the content type
+                    // this link will switch to, also reset the mode to daily, since it exists for
+                    // all "all-ages" modes.
+                    let current_ranking_type = ranking_types[current_mode];
+                    console.assert(current_ranking_type, current_mode);
+                    let switching_to_content_key = content_key_for(content, current_ages);
+                    if(current_ranking_type.content.indexOf(switching_to_content_key) == -1)
+                        args.query.set("mode", "daily");
+                },
+            });
+        }
+
+        let mode_container = this.ui.querySelector(".modes");
+
+        // Create the R18 and R18G buttons.  If we're on a selection where toggling this doesn't exist,
+        // pick a default.
+        for(let ages_toggle of ["r18", "r18g"])
+        {
+            let target_mode = current_mode;
+
+            let current_ranking_type = ranking_types[current_mode];
+            console.assert(current_ranking_type, current_mode);
+            let { content } = current_ranking_type;
+
+            let button = helpers.create_box_link({
+                label: ages_toggle.toUpperCase(),
+                popup: `Show ${ages_toggle.toUpperCase()} works`,
+                classes: [ages_toggle],
+                as_element: true,
+            });
+            mode_container.appendChild(button);
+
+            // If toggling this would put us in a mode that doesn't exist, default to "daily" for R18 and
+            // "weekly" for R18G, since those combinations always exist.  The buttons aren't disabled or
+            // removed since it makes it confusing to find them.
+            let content_key_for_mode = content_key_for(current_content, ages_toggle);
+            if(content.indexOf(content_key_for_mode) == -1)
+                target_mode = ages_toggle == "r18"? "daily":"weekly";
+
+            let mode_enabled = mode_with_ages(target_mode, ages_toggle);
+            let mode_disabled = mode_with_ages(target_mode, "all");
+
+            this.set_item(button, {
+                fields: {mode: mode_enabled},
+                toggle: true,
+                current_url: current_args.url,
+                classes: [ages_toggle], // only show if enabled
+                adjust_url: (args) => {
+                    // If we're in R18, clicking this would remove the mode field entirely.  Instead,
+                    // switch to the all-ages link.
+                    if(current_ages == ages_toggle)
+                        args.query.set("mode", mode_disabled);
+                }
+            });
+        }
+
+        // Create mode links for rankings that exist in the current content and ages selection.
+        for(let [mode, {content, label, popup}] of Object.entries(ranking_types))
+        {
+            console.assert(content, mode);
+
+            mode = mode_with_ages(mode, current_ages);
+
+            // Skip this mode if it's not available in the selected content and ages combination.
+            if(content.indexOf(content_key) == -1)
+                continue;
+
+            let button = helpers.create_box_link({
+                label,
+                popup,
+                as_element: true,
+            });
+            mode_container.appendChild(button);
+
+            this.set_item(button, {
+                fields: {mode},
+                default_values: {mode: "daily"},
+                current_url: current_args.url,
+            });
+        }
 
         return this.ui;
     }
 
     refresh_thumbnail_ui()
     {
-        this.set_item(this.ui.container, { type: "content-all", fields: {content: null} });
-        this.set_item(this.ui.container, { type: "content-illust", fields: {content: "illust"} });
-        this.set_item(this.ui.container, { type: "content-ugoira", fields: {content: "ugoira"} });
-        this.set_item(this.ui.container, { type: "content-manga", fields: {content: "manga"} });
-
-        this.set_item(this.ui.container, { type: "mode-daily", fields: {mode: null}, default_values: {mode: "daily"} });
-        this.set_item(this.ui.container, { type: "mode-daily-r18", fields: {mode: "daily_r18"} });
-        this.set_item(this.ui.container, { type: "mode-r18g", fields: {mode: "r18g"} });
-        this.set_item(this.ui.container, { type: "mode-ai", fields: {mode: "daily_ai"} });
-        this.set_item(this.ui.container, { type: "mode-ai-r18", fields: {mode: "daily_r18_ai"} });
-        this.set_item(this.ui.container, { type: "mode-weekly", fields: {mode: "weekly"} });
-        this.set_item(this.ui.container, { type: "mode-monthly", fields: {mode: "monthly"} });
-        this.set_item(this.ui.container, { type: "mode-rookie", fields: {mode: "rookie"} });
-        this.set_item(this.ui.container, { type: "mode-original", fields: {mode: "original"} });
-        this.set_item(this.ui.container, { type: "mode-male", fields: {mode: "male"} });
-        this.set_item(this.ui.container, { type: "mode-female", fields: {mode: "female"} });
-
         if(this.today_text)
-        this.ui.container.querySelector(".nav-today").innerText = this.today_text;
+            this.ui.container.querySelector(".nav-today").innerText = this.today_text;
 
         // This UI is greyed rather than hidden before we have the dates, so the UI doesn't
         // shift around as we load.
-        var yesterday = this.ui.container.querySelector(".nav-yesterday");
+        let yesterday = this.ui.container.querySelector(".nav-yesterday");
         helpers.set_class(yesterday, "disabled", this.prev_date == null);
         if(this.prev_date)
         {
@@ -1630,81 +1784,13 @@ ppixiv.data_sources.rankings = class extends data_source
             yesterday.href = url;
         }
 
-        var tomorrow = this.ui.container.querySelector(".nav-tomorrow");
+        let tomorrow = this.ui.container.querySelector(".nav-tomorrow");
         helpers.set_class(tomorrow, "disabled", this.next_date == null);
         if(this.next_date)
         {
             let url = new URL(this.url);
             url.searchParams.set("date", this.next_date);
             tomorrow.href = url;
-        }
-
-        // Not all combinations of content and mode exist.  For example, there's no ugoira
-        // monthly, and we'll get an error page if we load it.  Hide navigations that aren't
-        // available.  This isn't perfect: if you want to choose ugoira when you're on monthly
-        // you need to select a different time range first.  We could have the content links
-        // switch to daily if not available...
-        var available_combinations = [
-            "all/daily",
-            "all/daily_r18",
-            "all/daily_ai",
-            "all/daily_r18_ai",
-            "all/r18g",
-            "all/weekly",
-            "all/monthly",
-            "all/rookie",
-            "all/original",
-            "all/male",
-            "all/female",
-
-            "illust/daily",
-            "illust/daily_r18",
-            "illust/r18g",
-            "illust/weekly",
-            "illust/monthly",
-            "illust/rookie",
-
-            "ugoira/daily",
-            "ugoira/weekly",
-            "ugoira/daily_r18",
-
-            "manga/daily",
-            "manga/daily_r18",
-            "manga/r18g",
-            "manga/weekly",
-            "manga/monthly",
-            "manga/rookie",
-        ];
-
-        // Check each link in both checked-links sections.
-        for(var a of this.ui.container.querySelectorAll(".checked-links a"))
-        {
-            let url = new URL(a.href, this.url);
-            var link_content = url.searchParams.get("content") || "all";
-            var link_mode = url.searchParams.get("mode") || "daily";
-            var name = link_content + "/" + link_mode;
-
-            var available = available_combinations.indexOf(name) != -1;
-
-            var is_content_link = a.dataset.type.startsWith("content");
-            if(is_content_link)
-            {
-                // If this is a content link (eg. illustrations) and the combination of the
-                // current time range and this content type isn't available, make this link
-                // go to daily rather than hiding it, so all content types are always available
-                // and you don't have to switch time ranges just to select a different type.
-                if(!available)
-                {
-                    url.searchParams.delete("mode");
-                    a.href = url;
-                }
-            }
-            else
-            {
-                // If this is a mode link (eg. weekly) and it's not available, just hide
-                // the link.
-                a.hidden = !available;
-            }
         }
     }
 }
