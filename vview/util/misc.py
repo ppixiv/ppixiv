042a48be796564e9f32972c900d9b3888c2e7099
ppixiv <ppixiv@users.noreply.github.com>
2023-07-30 23:17:24 -0500

    Use PNG text info as the image description if it exists.

--- vview/util/misc.py
+++ vview/util/misc.py
@@ -183,6 +183,7 @@ def read_metadata(f, mime_type):
 
     result['width'] = img.size[0]
     result['height'] = img.size[1]
+    result['comment'] = img.info.get('parameters') or ''
 
     # PIL's parser for PNGs is very slow, so only support metadata from JPEG for now.
     if mime_type == 'image/jpeg' and hasattr(img, '_getexif'):
