987ddcc04d1abbe80fb6fc963dea3f3d8f3459cb
ppixiv <ppixiv@users.noreply.github.com>
2023-08-14 19:49:33 -0500

    Close WS connections with their status instead of letting aiohttp close it with a default.

--- vview/server/websockets.py
+++ vview/server/websockets.py
@@ -34,22 +34,32 @@ class WebsocketClientConnection:
         task = asyncio.current_task()
         task.set_name('WS-' + task.get_name())
 
-        log.info('WebSockets connection opened from %s (%s)', self.request.remote, self.browser_id)
+        log.info(f'WebSockets connection opened ({self.request.remote}: {self.browser_id})')
 
         # Add ourself to the connection list, so we'll receive broadcasts from other clients.
         self.websockets_connections.add(self)
 
+        close_code = aiohttp.WSCloseCode.OK
+        close_message = 'Closing connection'
         try:
             await self.loop()
+        except Exception as e:
+            close_code = aiohttp.WSCloseCode.INTERNAL_ERROR
+            close_message = f'Server error: {e}'
         finally:
             self.websockets_connections.remove(self)
+
+            error = self.ws.exception() or 'Connection closed'
+            log.info(f'WebSockets connection closed ({self.request.remote}: {self.browser_id}): {error}')
+
+        # Close the connection if it's still open.
+        await self.ws.close(code=close_code, message=close_message)
 
         return self.ws
 
     async def loop(self):
         async for msg in self.ws:
             if msg.type == aiohttp.WSMsgType.ERROR:
-                log.info('WebSockets connection closed: %s' % self.ws.exception())
                 break
 
             if msg.type != aiohttp.WSMsgType.TEXT:
