a331de0b1209a09947eb646455ff0dc48091948d
ppixiv <ppixiv@users.noreply.github.com>
2023-09-19 01:04:25 -0500

    Update the script permissions header.
    This adds api.cotrans.touhou.ai for translations.  While we're prompting users for this anyway,
    add i-cf.pximg.net too so downloading images from that image host works, and switch @include to @match.

--- web/startup/header.js
+++ web/startup/header.js
@@ -3,8 +3,7 @@
 // @author      ppixiv
 // @description Better Pixiv viewing | Fullscreen images | Faster searching | Bigger thumbnails | Download ugoira MKV | Ugoira seek bar | Download manga ZIP | One-click like, bookmark, follow | One-click zoom and pan
 // @homepage    https://github.com/ppixiv/ppixiv
-// @include     http://*.pixiv.net/*
-// @include     https://*.pixiv.net/*
+// @match       https://*.pixiv.net/*
 // @run-at      document-start
 // @grant       GM.xmlHttpRequest
 // @grant       GM_xmlhttpRequest
@@ -12,5 +11,7 @@
 // @grant       GM_getValue
 // @connect     pixiv.net
 // @connect     i.pximg.net
+// @connect     i-cf.pximg.net
+// @connect     api.cotrans.touhou.ai
 // @connect     self
 // @namespace   ppixiv
