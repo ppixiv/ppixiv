34e8b9d8bc54e0c59bbd45b1854e124a334ae50d
ppixiv <ppixiv@users.noreply.github.com>
2023-10-01 12:58:53 -0500

    Revert the header changes (not sure yet if these will be needed).

--- web/startup/header.js
+++ web/startup/header.js
@@ -3,14 +3,14 @@
 // @author      ppixiv
 // @description Better Pixiv viewing | Fullscreen images | Faster searching | Bigger thumbnails | Download ugoira MKV | Ugoira seek bar | Download manga ZIP | One-click like, bookmark, follow | One-click zoom and pan
 // @homepage    https://github.com/ppixiv/ppixiv
-// @match       https://*.pixiv.net/*
+// @include     http://*.pixiv.net/*
+// @include     https://*.pixiv.net/*
 // @run-at      document-start
 // @grant       GM.xmlHttpRequest
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @connect     pixiv.net
-// @connect     pximg.net
-// @connect     cotrans.touhou.ai
+// @connect     i.pximg.net
 // @connect     self
 // @namespace   ppixiv
