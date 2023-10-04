1593524929f80fde3fc858aaae3dad16830112f8
ppixiv <ppixiv@users.noreply.github.com>
2023-10-04 09:11:07 -0500

    Update the header, and use @connect * instead of listing the translation API directly.

--- web/startup/header.js
+++ web/startup/header.js
@@ -1,16 +1,21 @@
 // ==UserScript==
 // @name        ppixiv for Pixiv
 // @author      ppixiv
+// @namespace   ppixiv
 // @description Better Pixiv viewing | Fullscreen images | Faster searching | Bigger thumbnails | Download ugoira MKV | Ugoira seek bar | Download manga ZIP | One-click like, bookmark, follow | One-click zoom and pan
 // @homepage    https://github.com/ppixiv/ppixiv
-// @include     http://*.pixiv.net/*
-// @include     https://*.pixiv.net/*
+// @match       https://*.pixiv.net/*
 // @run-at      document-start
+// @icon        https://ppixiv.org/ppixiv.png
 // @grant       GM.xmlHttpRequest
-// @grant       GM_xmlhttpRequest
-// @grant       GM_setValue
-// @grant       GM_getValue
+// @grant       GM.setValue
+// @grant       GM.getValue
 // @connect     pixiv.net
-// @connect     i.pximg.net
+// @connect     pximg.net
 // @connect     self
-// @namespace   ppixiv
+//
+// @comment     Note: this doesn't actually give the script access everywhere.  It just lets us request
+// @comment     access to new domains at runtime.  If you use a feature that needs access to another site,
+// @comment     the script manager will ask for permission the first time.
+// @connect     *
+//
