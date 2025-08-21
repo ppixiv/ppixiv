0c318ee62018328d3786d43d90f5adcb962365a5
ppixiv <ppixiv@users.noreply.github.com>
2025-08-20 19:42:40 -0500

    Add missing constants.js (#127).

---
+++ web/vview/misc/constants.js
@@ -0,0 +1,3 @@
+// The minimum number of pixels that a pointer must move before we consider it a drag.
+// This is only used on Android, since iOS does this for us.
+export const DRAG_THRESHOLD = 3; // px
